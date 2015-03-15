041e5b0543411f45883240a5dbc575bfd3f0478d
hikoLab <hikoLab@users.noreply.github.com>
2015-03-15 22:47:37 +0900

    Modify year(), month() to match Processing's

--- pebcessing_lib.c
+++ pebcessing_lib.c
@@ -177,15 +177,15 @@ int year()
 {
   time_t now = time(NULL);
   struct tm *current_time = localtime(&now);
-  return current_time->tm_year;
+  return current_time->tm_year + 1900;
 }
 
-// return value : 0-11
+// return value : 1-12
 int month()
 {
   time_t now = time(NULL);
   struct tm *current_time = localtime(&now);
-  return current_time->tm_mon;
+  return current_time->tm_mon + 1;
 }
 
 // return value : 1-31
@@ -224,7 +224,7 @@ int minute()
   return current_time->tm_min;
 }
 
-// return value : 0-61
+// return value : 0-61 (60 or 61 are leap-seconds)
 int second()
 {
   time_t now = time(NULL);
