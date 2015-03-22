8c9e65c153592e24c0f676735fa8b8daa1ebfdd6
hikoLab <hikoLab@users.noreply.github.com>
2015-03-22 19:07:02 +0900

    Make small changes to sketch.c

--- src/sketch.c
+++ src/sketch.c
@@ -15,37 +15,50 @@ void setup()
 
 void draw()
 {
-  float x = random(0, width);
-  float y = random(0, height);
-  float r = random(10, 25);
-  ellipse(x, y, r, r);
+  // Draw rectangles at random positions
+  float x = random(-20, width);
+  float y = random(-20, height);
+  float r = random(10, 30);
+  rect(x, y, r, r);
 }
 
 /*
+// The secondEvent() function is called every second.
+// Define ENABLE_SECOND_EVENT in settings.h, if you use this function.
 void secondEvent()
 {
 }
 */
 
 /*
+// The minuteEvent() function is called every minute.
+// Define ENABLE_MINUTE_EVENT in settings.h, if you use this function.
 void minuteEvent()
 {
 }
 */
 
 /*
+// The hourEvent() function is called every hour.
+// Define ENABLE_HOUR_EVENT in settings.h, if you use this function.
 void hourEvent()
 {
 }
 */
 
 /*
+// The dayEvent() function is called every day.
+// Define ENABLE_DAY_EVENT in settings.h, if you use this function.
 void dayEvent()
 {
 }
 */
 
 /*
+// The keyPressed() function is called when a button is pressed,
+// and the keyReleased() function is called when a button is released.
+// Define ENABLE_KEY_EVENT in settings.h, if you use these functions.
+// These functions are valid only when this code is built as a watchapp.
 void keyPressed()
 {
 }
